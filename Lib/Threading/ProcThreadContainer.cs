479607eea4015090543d2ad5ebb6e4acc0304c54
Cameron Gallichan <camerongallichan@gmail.com>
2025-02-27 15:33:38 +0000

    add thread pool queue class with test

---
+++ Lib/Threading/ProcThreadContainer.cs
@@ -0,0 +1,13 @@
+﻿namespace Lib.Threading;
+
+using System;
+
+public class ProcThreadContainer
+{
+    // This thread procedure performs the task.
+    public static void ThreadProc(Object? stateInfo)
+    {
+        // No state object was passed to QueueUserWorkItem, so stateInfo is null.
+        Console.WriteLine("Hello from the thread pool.");
+    }
+}
---
+++ LibTests/Threading/ProcThreadContainerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace LibTests.Threading;
+
+using Lib.Threading;
+
+[TestClass]
+public sealed class ProcThreadContainerTests
+{
+    [TestMethod]
+    public void ThreadQueueTest()
+    {
+        // Queue the task.
+        ThreadPool.QueueUserWorkItem(ProcThreadContainer.ThreadProc);
+        Console.WriteLine("Main thread does some work, then sleeps.");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Main thread exits.");
+    }
+}
